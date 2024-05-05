fdb5536975616b00e779bf0e4e0f88bc47087c1f
DraconicFal <160436571+DraconicFal@users.noreply.github.com>
2024-05-05 16:35:01 -0400

    s

--- Assets/Scripts/ClipMainScript.cs
+++ Assets/Scripts/ClipMainScript.cs
@@ -15,12 +15,14 @@ public class ClipMainScript : MonoBehaviour
     public float duration = 2; //should be in seconds, does the action in the alotted time.
     public float startTime = 0;
     RectTransform rectTransform;
+    float oldDuration;
 
     Vector2 oldSize;
 
     // Start is called before the first frame update
     void Start()
     {
+        oldDuration = duration * 100f;
         rectTransform = GetComponent<RectTransform>();
         oldSize = rectTransform.sizeDelta;
     }
@@ -28,6 +30,16 @@ public class ClipMainScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (duration * 100 != oldDuration)
+        {
+            updateSize();
+        }
+    }
+
+    void updateSize()
+    {
         rectTransform.sizeDelta = new Vector2(duration * 100, oldSize.y);
+        //rectTransform.position = new Vector2(rectTransform.position.x + Mathf.Abs((oldDuration - duration) * 1 / 2), rectTransform.position.y);
+        oldDuration = duration * 100f;
     }
 }
