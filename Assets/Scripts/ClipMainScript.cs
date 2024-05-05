ae636068e546e4f824cce18a899d1ae2044f1ea3
DraconicFal <160436571+DraconicFal@users.noreply.github.com>
2024-05-05 12:22:53 -0400

    Size changing WOW

--- Assets/Scripts/ClipMainScript.cs
+++ Assets/Scripts/ClipMainScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClipMainScript : MonoBehaviour
 {
@@ -11,18 +12,22 @@ public class ClipMainScript : MonoBehaviour
         Claw
     }
     public ClipType clipType;
-    public float duration = 1; //should be in seconds, does the action in the alotted time.
+    public float duration = 2; //should be in seconds, does the action in the alotted time.
     public float startTime = 0;
+    RectTransform rectTransform;
+
+    Vector2 oldSize;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        oldSize = rectTransform.sizeDelta;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rectTransform.sizeDelta = new Vector2(duration * 100, oldSize.y);
     }
 }
